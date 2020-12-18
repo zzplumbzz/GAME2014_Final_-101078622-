0f52538cd6e31491e7daaff8809afec6418ea308
Nicolas Plumb <nicolasplumb@yahoo.com>
2020-12-17 20:07:14 -0500

    initial commit

---
+++ GAME2014-F2020-FinalTest/Assets/_Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Bullet Related")]
+    public int MaxBullets;
+    public BulletType enemyBulletType;
+    public BulletType playerBulletType;
+
+    [Header("Moving Platforms")] 
+    public List<MovingPlatformController> movingPlatforms;
+
+    [Header("Shrinking Platforms")]
+    public List<ShrinkingPlatform> shrinkingPlatforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingPlatforms = FindObjectsOfType<MovingPlatformController>().ToList();
+        shrinkingPlatforms = FindObjectsOfType<ShrinkingPlatform>().ToList();
+
+
+        // Kickoff the BulletManager
+        BulletManager.Instance().Init(MaxBullets, enemyBulletType, playerBulletType);
+    }
+
+    public void ResetAllPlatforms()
+    {
+        foreach (var platform in movingPlatforms)
+        {
+            platform.Reset();
+        }
+
+        foreach (var platform in shrinkingPlatforms)
+        {
+            //platform.Reset();
+        }
+    }
+
+}
---
+++ GAME2014-F2020-FinalTest/Assets/_Scripts/ShrinkingPlatform.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SFX
+{
+    SHRINKSFX,
+    GROWSFX
+}
+public class ShrinkingPlatform : MonoBehaviour
+{
+    
+    public AudioSource[] sfx;
+    public bool isActive;
+    public bool floating;
+    public float maxHeight, minHeight;
+    public float shrinkSpeed;
+    public float platformSpeed;
+    public float yPos;
+    
+    private void Start()
+    {
+        sfx = GetComponents<AudioSource>();
+        isActive = false;
+        
+    }
+    void Update()
+    {
+        _Move();
+       platformActivated();
+    }
+
+    private void platformActivated()
+    {
+        sfx[(int)SFX.SHRINKSFX].Play();
+        if (isActive)
+        {
+            if (gameObject.transform.localScale.x < 0.0f)
+            {
+                gameObject.transform.localScale = new Vector2(0.0f, transform.localScale.y);
+            }else
+            {
+                gameObject.transform.localScale = new Vector2(transform.localScale.x - 5 * Time.deltaTime, transform.localScale.y);
+            }
+        }else
+        {
+           if (gameObject.transform.localScale.x != 9.0f)
+            {
+                sfx[(int)SFX.GROWSFX].Play();
+                StartCoroutine("ResetPlatform");
+            }
+        }
+    }
+
+    private IEnumerator ResetPlatform()
+    {
+        yield return new WaitForSeconds(2.0f);
+        gameObject.transform.localScale = new Vector2(15, 5);
+    }
+
+    private void _Move()
+    {
+        _Movement();
+        if (floating)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + platformSpeed * Time.deltaTime);
+        }
+        else if (floating)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - platformSpeed * Time.deltaTime);
+        }
+    }
+
+    private void _Movement()
+    {
+        if ((isActive))
+        {
+            sfx[(int)SFX.SHRINKSFX].Play();
+            if (transform.position.y > maxHeight)
+            {
+                floating = false;
+            }
+            else if (transform.position.y < minHeight)
+            {
+                floating = true;
+            }
+        }
+       
+
+    }
+}
+    
+
+    
+
